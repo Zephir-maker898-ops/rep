619153cefd26bd7868be33ffb64f1b99b8acba54
Zephir-maker898-ops <dolgorukov2001@list.ru>
2021-06-06 13:08:11 +0300

    v

--- main.py
+++ main.py
@@ -15,27 +15,26 @@ def repeat_all_messages(message):  # Название функции не игр
     if message.text == 'порадовать себя)':
         bot.send_message(message.chat.id, random.choice(config.array), reply_markup=markup)
 
-# это функция отправки сообщений по таймеру
-def check_send_messages():
-    while True:
-        # ваш код проверки времени и отправки сообщений по таймеру
-       if 
-        # пауза между проверками, чтобы не загружать процессор
-        time.sleep(60)
-
-# а теперь запускаем проверку в отдельном потоке
-p1 = Process(target=check_send_messages, args=())
-p1.start()
-
-# а это включение бота на прием сообщений
-# обернуто в try, потому что если Telegram сервер станет недоступен, возможен крэш
-while True:
-    try:
-        bot.infinity_polling()
-    except Exception as e:
-        print(e)
-        # повторяем через 15 секунд в случае недоступности сервера Telegram
-        time.sleep(15)
+# # это функция отправки сообщений по таймеру
+# def check_send_messages():
+#     while True:
+#         # ваш код проверки времени и отправки сообщений по таймеру
+#         # пауза между проверками, чтобы не загружать процессор
+#         time.sleep(60)
+#
+# # а теперь запускаем проверку в отдельном потоке
+# p1 = Process(target=check_send_messages, args=())
+# p1.start()
+#
+# # а это включение бота на прием сообщений
+# # обернуто в try, потому что если Telegram сервер станет недоступен, возможен крэш
+# while True:
+#     try:
+#         bot.infinity_polling()
+#     except Exception as e:
+#         print(e)
+#         # повторяем через 15 секунд в случае недоступности сервера Telegram
+#         time.sleep(15)
 
 
 if __name__ == '__main__':
