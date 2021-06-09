19a9fc3de9dbeb79148c009dcf8384d869958380
Zephir-maker898-ops <dolgorukov2001@list.ru>
2021-06-09 23:06:50 +0300

    rand

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         markup = types.ReplyKeyboardMarkup(True)
         markup.add('порадовать себя)')
         if message.text == 'порадовать себя)':
-            bot.send_message(message.chat.id, random.choice(config.array_text)+random.choice(config.array_smiles), reply_markup=markup)
+            bot.send_message(message.chat.id, random.choice(test_timer.at)+random.choice(test_timer.ass)+random.choice(test_timer.ass), reply_markup=markup)
             print("pressed")
     # а теперь запускаем проверку в отдельном потоке
 
@@ -28,5 +28,5 @@ if __name__ == '__main__':
         except Exception as e:
             print(e)
             # повторяем через 15 секунд в случае недоступности сервера Telegram
-            time.sleep(15)
+            time.sleep(14)
 
